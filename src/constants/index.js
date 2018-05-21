90ac9ce8da38ac22b45e1a64ff98048f92178516
Liam <liam.bowers@adp.com>
2018-05-21 10:32:12 -0400

    app key

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,7 +1,7 @@
 export default {
   DATA_RECEIVED: 'DATA_RECEIVED',
   TOGGLE_SELECTED_ROUTE: 'TOGGLE_SELECTED_ROUTE',
-  RTM_APP_KEY: '36b5259FAddd1c7B768c19Ddea606Dcf',
+  RTM_APP_KEY: 'insert your app key here',
   RTM_ENDPOINT: 'wss://open-data.api.satori.com',
   RTM_CHANNEL: 'transportation',
   RTM_LOCALE: 'trimet'
