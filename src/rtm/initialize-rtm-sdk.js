b1051c19c3ebc3c03035762307472e1334424053
Liam <liam.bowers@adp.com>
2018-05-21 10:26:07 -0400

    chore: update constants

--- src/rtm/initialize-rtm-sdk.js
+++ src/rtm/initialize-rtm-sdk.js
@@ -1,22 +1,23 @@
 import RTM from 'satori-rtm-sdk';
+import constants from '../constants';
 
-const RTM_ENDPOINT = 'wss://open-data.api.satori.com';
-const APP_KEY = '36b5259FAddd1c7B768c19Ddea606Dcf';
-const CHANNEL = 'transportation';
-const LOCALE = 'trimet';
+const { RTM_APP_KEY, RTM_CHANNEL, RTM_ENDPOINT, RTM_LOCALE } = constants;
 
 function initializeRTMClient(dispatchData) {
-  const client = new RTM(RTM_ENDPOINT, APP_KEY);
+  const client = new RTM(RTM_ENDPOINT, RTM_APP_KEY);
 
   client.on('enter-connected', function() {
     console.log('Connected to Satori RTM!');
   });
 
-  const subscription = client.subscribe(CHANNEL, RTM.SubscriptionMode.SIMPLE);
+  const subscription = client.subscribe(
+    RTM_CHANNEL,
+    RTM.SubscriptionMode.SIMPLE
+  );
 
   subscription.on('rtm/subscription/data', pdu => {
     const data = pdu.body.messages.filter(
-      message => message.header['user-data'] === LOCALE
+      message => message.header['user-data'] === RTM_LOCALE
     );
 
     if (data.length > 0) {
